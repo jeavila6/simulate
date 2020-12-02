5e5a65d959a214badd7fd6f60aed34a0a10c7ac2
Jonathan Avila <jonathan.edav@gmail.com>
2020-12-02 13:10:02 -0700

    Add baseline

--- simulate.py
+++ simulate.py
@@ -13,11 +13,43 @@ class State:
     def is_terminal_state(self):
         return self.choices == None
 
-
 class Choice:
     def __init__(self, out_state, weight=1):
         self.out_state = out_state
         self.weight = weight
+
+def run_baseline(start_state, cost_per_step):
+
+    payoff_total = 0
+    steps_taken = 0
+    curr_state = start_state
+
+    while not curr_state.is_terminal_state():
+
+        # Pick a choice based on their weights. If all choices have the same,
+        # weight, assume the choice is being made by the defender and pick the
+        # choice with the highest payoff instead.
+        choices = curr_state.choices
+        weights = [final_choice.weight for final_choice in choices]
+        all_weights_equal = all(w == weights[0] for w in weights)
+        if all_weights_equal:
+            final_choice = choices[0]
+            for choice in choices:
+                if choice.out_state.payoff >= final_choice.out_state.payoff:
+                    final_choice = choice
+        else:
+            final_choice = random.choices(choices, weights)[0]
+        next_state = final_choice.out_state
+
+        payoff = next_state.payoff
+        payoff_total += payoff
+        steps_taken += 1
+        curr_state = next_state
+
+    cost_for_steps = cost_per_step * steps_taken
+    payoff_total += cost_for_steps
+
+    return payoff_total
 
 
 def run_episode(start_state, cost_per_step):
@@ -29,7 +61,8 @@ def run_episode(start_state, cost_per_step):
 
     while not curr_state.is_terminal_state():
 
-        # Make a decision.
+        # Pick a choice based on their weights. If all choices have the same,
+        # weight, the choice is random.
         choices = curr_state.choices
         weights = [final_choice.weight for final_choice in choices]
         final_choice = random.choices(choices, weights)[0]
@@ -110,18 +143,27 @@ if __name__ == "__main__":
     d1 = State("d1: attack happened", cost_attack, [Choice(d3), Choice(d4)])
     d0 = State("d0: vulnerability found", 0, [Choice(d1, 0.8), Choice(d2, 0.2)])
 
-    N_EPISODES = 10000
     LEARNING_RATE = 0.1
+    N_EPISODES = 100000
     START_STATE = d0
     STEP_COST = -1
 
+    # Run baseline.
+    total_payoff = 0
+    for i in range(N_EPISODES):
+        total_payoff += run_baseline(START_STATE, STEP_COST)
+    average_payoff = total_payoff / N_EPISODES
+    print(f"Basline average payoff = {average_payoff}")
+
+    # Run simulations.
     for i in range(N_EPISODES):
         episode_payoff, states_visited = run_episode(START_STATE, STEP_COST)
         for state in states_visited:
             state.value = state.value + LEARNING_RATE * (episode_payoff - state.value)
 
+    # Print simulation parameters and terminal states' values.
     print(f"N_EPISODES={N_EPISODES}, LEARNING_RATE={LEARNING_RATE}, START_STATE=\"{START_STATE.name}\", STEP_COST={STEP_COST}")
     print("[terminal_state_id: state_name] = value")
     for state in State.all_states:
         if state.is_terminal_state():
-            print(f"[{state.name}] = {float(state.value):.5}")
+            print(f"[{state.name}] = {float(state.value):.5}")    
